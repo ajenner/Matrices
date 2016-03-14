0cde06e5ca647a007e7e4470bd87a7bc38accb61
Aaron Jenner <aaronjenner13@gmail.com>
2016-03-14 17:39:38 +0000

    added first pragma

--- complex-matmul-harness.c
+++ complex-matmul-harness.c
@@ -153,8 +153,24 @@ void matmul(struct complex ** A, struct complex ** B, struct complex ** C, int a
 
 /* the fast version of matmul written by the team */
 void team_matmul(struct complex ** A, struct complex ** B, struct complex ** C, int a_rows, int a_cols, int b_cols) {
-  //replace this
-  matmul(A, B, C, a_rows, a_cols, b_cols);
+  int i, j, k;
+#pragma omp parallel for
+  for ( i = 0; i < a_dim1; i++ ) {
+    for( j = 0; j < b_dim2; j++ ) {
+      struct complex sum;
+      sum.real = 0.0;
+      sum.imag = 0.0;
+      for ( k = 0; k < a_dim2; k++ ) {
+        // the following code does: sum += A[i][k] * B[k][j];
+        struct complex product;
+        product.real = A[i][k].real * B[k][j].real - A[i][k].imag * B[k][j].imag;
+        product.imag = A[i][k].real * B[k][j].imag + A[i][k].imag * B[k][j].real;
+        sum.real += product.real;
+        sum.imag += product.imag;
+      }
+      C[i][j] = sum;
+    }
+  }
 }
 
 long long time_diff(struct timeval * start, struct timeval * end) {
